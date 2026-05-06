a819dca5833df9e34e09b6fc56f2b5dc93391e86
isaac <isaacloff@gmail.com>
2026-05-06 18:21:43 +0100

    tick checked add

--- src/event_clock/clock/tick/mod.rs
+++ src/event_clock/clock/tick/mod.rs
@@ -1,14 +1,18 @@
-#[derive(Default)]
+#[derive(Default, PartialEq, PartialOrd)]
 pub struct Tick {
     accumulator: usize
 }
 
 impl Tick {
-    pub fn increment(&mut self) -> Option<Tick> {
+    pub fn increment(&mut self) -> Option<Self> {
         let old = Tick { accumulator: self.accumulator };
         
-        self.accumulator = self.accumulator.checked_add(1)?;
-
+        *self = self.checked_add(&Self { accumulator: 1 })?;
+    
         Some(old)
+    }
+    
+    pub fn checked_add(&self, rhs: &Self) -> Option<Self> {
+        self.accumulator.checked_add(rhs.accumulator).map(|tick| Self { accumulator: tick })
     }
 }
