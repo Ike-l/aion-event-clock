b897c05fc8c81d4aff097a1ff0683360d6c6bd90
isaac <isaacloff@gmail.com>
2026-05-06 18:21:29 +0100

    clock instant checked add

--- src/event_clock/clock/clock_instant/mod.rs
+++ src/event_clock/clock/clock_instant/mod.rs
@@ -1,7 +1,21 @@
-use crate::prelude::{Tick};
+use crate::prelude::{Tick, ClockDuration};
 use std::time::Instant;
 
 pub enum ClockInstant {
     Tick(Tick),
     Time(Instant)
+}
+
+impl ClockInstant {
+    pub fn checked_add(&self, clock_duration: &ClockDuration) -> Option<Option<Self>> {
+        match (self, clock_duration) {
+            (Self::Tick(tick_instant), ClockDuration::Tick(tick_duration)) => {
+                Some(tick_instant.checked_add(tick_duration).map(|tick| Self::Tick(tick)))
+            },
+            (Self::Time(instant), ClockDuration::Duration(duration)) => {
+                Some(instant.checked_add(*duration).map(|instant| Self::Time(instant)))
+            }
+            _ => None
+        }
+    }
 }
