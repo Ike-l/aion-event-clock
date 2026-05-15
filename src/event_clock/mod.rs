f23e85e85849f5f0fb470a87a3e245b097801386
isaac <isaacloff@gmail.com>
2026-05-15 20:17:23 +0100

    use world and query

--- src/event_clock/mod.rs
+++ src/event_clock/mod.rs
@@ -2,17 +2,18 @@
 // Event History
 // Can check with Event History
 
-use std::{collections::HashSet, sync::Arc};
+use std::{collections::{HashMap, HashSet}, sync::Arc};
 
+use aion_ecs::prelude::{Query, UNIQUE_WORLD_ACCESS_BUILDER, WORLD_RESOURCE_ID, World};
 use aion_event::prelude::{EventBuffer, EventHistory, EventSystem};
-use aion_program::prelude::ProgramRegistry;
+use aion_program::prelude::{ProgramRegistry, ProgramRegistryResolveWithInsert, Resource, Unique};
+use hecs::Entity;
 
-use crate::prelude::{Timer, get_mut_current_clock, get_clock_registry, get_mut_timer_registry};
+use crate::prelude::{Clock, Timer, get_mut_current_clock};
 
 pub mod clock;
 pub mod clock_capture;
-pub mod timer_registry;
-pub mod clock_registry;
+pub mod timer;
 
 pub struct EventClock;
 
@@ -32,44 +33,69 @@ impl EventSystem for EventClock {
 
         let current_clock = current_clock.as_ref();
 
-        let triggered_clocks = match get_clock_registry(program_registry) {
-            Ok(Ok(Ok(clock_registry))) => {
-                Some(clock_registry.as_ref().iter().filter(|clock| clock.triggered(current_events)).cloned().collect::<HashSet<_>>())
-            },
-            _ => None
-        };
-
-        match get_mut_timer_registry(program_registry) {
-            Ok(Ok(Ok(mut timer_registry))) => {
-                let timer_registry = timer_registry.as_mut();
-
-                let mut continuing_clocks = HashSet::new();
-
-                for mut timer in timer_registry.drain() {
+        let mut dead_timers = HashSet::new();
+        {
+            let timers = program_registry.resolve::<Query<(Entity, &mut Timer)>>(vec![]);
+            if let Ok(Ok(mut timers)) = timers {
+                for (entity, timer) in timers.borrow().iter() {
                     if timer.alive(current_clock) {
                         if timer.elapsed(current_clock) {
                             if let Some(alert) = timer.alert() {
                                 event_buffer.insert(alert.clone());
                             }
                         }
-    
-                        continuing_clocks.insert(timer);
                     } else {
                         if let Some(final_alert) = timer.final_alert() {
                             event_buffer.insert(final_alert.clone());
                         }
+
+                        dead_timers.insert(entity);
                     }
                 }
+            }
+        }
 
-                // Adds later because we know if they have just been added then they can't have elapsed
-                if let Some(triggered_clocks) = triggered_clocks {
-                    timer_registry.extend(triggered_clocks.into_iter().map(|triggered_clock| Timer::new(triggered_clock, current_clock.clone())));
+        {
+            let world = program_registry.resolve_with_insert::<Unique<World>>(vec![UNIQUE_WORLD_ACCESS_BUILDER], ProgramRegistryResolveWithInsert {
+                resource: Some(Box::new(|| Resource::new(World::default()))),
+                resource_id: Some(WORLD_RESOURCE_ID),
+                ..Default::default()
+            }).expect("Resource and ResourceId are Some");
+    
+            {
+                if let Ok(Ok(Ok(mut world))) = world {
+                    for dead_timer in dead_timers {                    
+                        let _ = world.as_mut().remove::<(Timer,)>(dead_timer);
+                    }
                 }
+            }
+        }
 
-                timer_registry.extend(continuing_clocks);
-            },
-            _ => ()
-        };
+        let mut triggered_clocks = HashMap::new();
+        {    
+            let clocks = program_registry.resolve::<Query<(Entity, &Clock)>>(vec![]);
+            if let Ok(Ok(mut clocks)) = clocks {
+                for (entity, clock) in clocks.borrow().iter() {
+                    if clock.triggered(current_events) {
+                        triggered_clocks.insert(entity, Timer::new(clock.clone(), current_clock.clone()));
+                    }
+                }
+            }
+        }
+
+        {
+            let world = program_registry.resolve_with_insert::<Unique<World>>(vec![UNIQUE_WORLD_ACCESS_BUILDER], ProgramRegistryResolveWithInsert {
+                resource: Some(Box::new(|| Resource::new(World::default()))),
+                resource_id: Some(WORLD_RESOURCE_ID),
+                ..Default::default()
+            }).expect("Resource and ResourceId are Some");
+            
+            if let Ok(Ok(Ok(mut world))) = world {
+                for (entity, timer) in triggered_clocks {
+                    let _ = world.as_mut().insert(entity, (timer,));
+                }
+            }
+        }
 
         event_buffer
     }
--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,8 @@ pub mod prelude {
                 ClockCapture,
                 get_mut_current_clock
             },
-            timer_registry::{
-                TIMER_REGISTRY_RESOURCE_ID,
-                TIMER_REGISTRY_ACCESS_BUILDER,
-                TimerRegistry,
-                get_mut_timer_registry,
-                timer::{
+            timer::{
                     Timer
-                }
-            },
-            clock_registry::{
-                CLOCK_REGISTRY_ACCESS_BUILDER,
-                CLOCK_REGISTRY_RESOURCE_ID,
-                ClockRegistry,
-                get_clock_registry
             },
             clock::{
                 Clock,
